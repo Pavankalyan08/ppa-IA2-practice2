146c76c21a2080656127ad6b2ac4634a46df5fe0
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-02-09 11:26:05 +0000

    p1original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
-
+#include<stdio.h>
+#include<math.h>
+void input(float*base,float*height)
+{
+  printf("enter the base\n");
+  scanf("%f");
+  printf("enter the height\n");
+  scanf("%f");
+}
+void find_area(float base,float height,float*area)
+{
+  *area=base*height/2;
+}
+void output(float base,float height,float area)
+{
+  printf("area of %f and %f is %f \n",base,height,area);
+}
+float main()
+{
+  float x,y,z;
+  x=input();
+  y=input();
+  z= find_area(x,y);
+  output(x,y,z);
+  return 0;
 }
